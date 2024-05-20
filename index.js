c94d036baa092fefb598e0f96f013a1e5afc7096
TamizhanbanT <143093806+TamizhanbanT@users.noreply.github.com>
2024-05-20 17:20:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.get('/api/reset/:token', async (req, res) => {
       return res.status(404).json({ error: 'Invalid or expired token' });
     }
       await pass.save()
-    res.json({ message: 'Token validated', userId: user._id ,Token:user.token });
+    res.json({ message: 'Token validated', userId: user._id  });
   } catch (error) {
     console.error('Error validating token:', error);
     res.status(500).json({ error: 'Failed to validate token' });
